f2cfbdd707e368fc9a602713131384affe16ee3a
Juanjo Bazán <jjbazan@gmail.com>
2010-11-06 23:28:53 +0100

    ITU morse code

--- lib/morse_characters.rb
+++ lib/morse_characters.rb
@@ -1,6 +1,6 @@
 module MorseCharacters
   chars = %w{a b c d e f g h i j k l m n ñ o p q r s t u v w x y z 0 1 2 3 4 5 6 7 8 9 . , ? "} << " "
-  morse = %w{a b c d e f g h i j k l m n ñ o p q r s t u v w x y z 0 1 2 3 4 5 6 7 8 9 . , ? "} << " "
+  morse = %w{.- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --.-- --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --.. ----- .---- ..--- ...-- ....- ..... -.... --... ---.. ----. .-.-.- --..-- ..--.. .-..-.} << "       "
   
   
   MORSE_TO_LETTERS = {}
