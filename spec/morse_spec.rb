7259f63830602d3c2024944020a2cf1a5782f6a3
Juanjo Bazán <jjbazan@gmail.com>
2015-11-22 21:23:02 +0100

    updates spec syntax from should to expect

--- spec/morse_spec.rb
+++ spec/morse_spec.rb
@@ -2,52 +2,51 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
 describe Telegraph::MorseTransmission do
-  
+
   describe 'Custom Morse Transmission' do
     it "uses default signal characters if none provided" do
       morser = Telegraph::MorseTransmission.new
-      morser.text_to_morse("ET").should == ". -"
-      morser.text_to_morse("ET").should == "#{Telegraph::DOT} #{Telegraph::DASH}"
+      expect(morser.text_to_morse("ET")).to eq ". -"
+      expect(morser.text_to_morse("ET")).to eq "#{Telegraph::DOT} #{Telegraph::DASH}"
     end
-    
+
     it "uses Telegraph methods if default signal characters" do
       morser = Telegraph::MorseTransmission.new
-      morser.text_to_morse("Winston is back").should == Telegraph.text_to_morse("Winston is back")
-      morser.morse_to_text(".... . .-.. .-.. ---        .-- --- .-. .-.. -..").should == 
-             Telegraph.morse_to_text(".... . .-.. .-.. ---        .-- --- .-. .-.. -..")
-      morser.text_to_morse("Winston is back").should == Telegraph.text_to_morse("Winston is back")
-      morser.error.should == Telegraph::Error
-      morser.understood.should == Telegraph::Understood
-      morser.invitation_to_transmit.should == Telegraph::Invitation_to_transmit
-      morser.wait.should == Telegraph::Wait
-      morser.end_of_work.should == Telegraph::End_of_work
-      morser.starting_signal.should == Telegraph::Starting_signal
+      expect(morser.text_to_morse("Winston is back")).to eq Telegraph.text_to_morse("Winston is back")
+      expect(morser.morse_to_text(".... . .-.. .-.. ---        .-- --- .-. .-.. -..")).to eq Telegraph.morse_to_text(".... . .-.. .-.. ---        .-- --- .-. .-.. -..")
+      expect(morser.text_to_morse("Winston is back")).to eq Telegraph.text_to_morse("Winston is back")
+      expect(morser.error).to eq Telegraph::Error
+      expect(morser.understood).to eq Telegraph::Understood
+      expect(morser.invitation_to_transmit).to eq Telegraph::Invitation_to_transmit
+      expect(morser.wait).to eq Telegraph::Wait
+      expect(morser.end_of_work).to eq Telegraph::End_of_work
+      expect(morser.starting_signal).to eq Telegraph::Starting_signal
     end
-    
+
     it "uses default signal if no provided signal char" do
       morser = Telegraph::MorseTransmission.new :short => 's'
-      morser.text_to_morse("ET").should == "s -"
-      morser.text_to_morse("ET").should == "s #{Telegraph::DASH}"
-      
-      
+      expect(morser.text_to_morse("ET")).to eq "s -"
+      expect(morser.text_to_morse("ET")).to eq "s #{Telegraph::DASH}"
+
+
       morser = Telegraph::MorseTransmission.new :long => 'j'
-      morser.text_to_morse("ET").should == ". j"
-      morser.text_to_morse("ET").should == "#{Telegraph::DOT} j"
+      expect(morser.text_to_morse("ET")).to eq ". j"
+      expect(morser.text_to_morse("ET")).to eq "#{Telegraph::DOT} j"
     end
-    
+
     it "write signal characters provided by user" do
       morser = Telegraph::MorseTransmission.new(:short => '3', :long => 'X')
-      morser.text_to_morse("ET").should == "3 X"
-      morser.text_to_morse("ET").should_not == Telegraph.text_to_morse("ET")
-      morser.text_to_morse("Winston is back").should == "3XX 33 X3 333 X XXX X3       33 333       X333 3X X3X3 X3X"
-      morser.error.should == "33333333"
+      expect(morser.text_to_morse("ET")).to eq "3 X"
+      expect(morser.text_to_morse("ET")).to_not eq Telegraph.text_to_morse("ET")
+      expect(morser.text_to_morse("Winston is back")).to eq "3XX 33 X3 333 X XXX X3       33 333       X333 3X X3X3 X3X"
+      expect(morser.error).to eq "33333333"
     end
-    
+
     it "reads signal characters provided by user" do
       morser = Telegraph::MorseTransmission.new(:short => '%', :long => '+')
-      morser.morse_to_text("% +").should == "et"
-      morser.morse_to_text("%%%%+ %+%+% %++++ +++++").should == "4+10"
+      expect(morser.morse_to_text("% +")).to eq "et"
+      expect(morser.morse_to_text("%%%%+ %+%+% %++++ +++++")).to eq "4+10"
     end
   end
-  
+
 end
--- spec/telegraph_spec.rb
+++ spec/telegraph_spec.rb
@@ -2,78 +2,77 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
 describe Telegraph do
-  
+
   describe 'Morse writer' do
     it "creates morse code from a simple text" do
-      Telegraph.text_to_morse("A patient waiter is no loser.").should == 
-      ".-       .--. .- - .. . -. -       .-- .- .. - . .-.       .. ...       -. ---       .-.. --- ... . .-. .-.-.-"
+      expect(Telegraph.text_to_morse("A patient waiter is no loser.")).to eq(
+      ".-       .--. .- - .. . -. -       .-- .- .. - . .-.       .. ...       -. ---       .-.. --- ... . .-. .-.-.-")
     end
-    
+
     it "undestands some international characters (if ruby -v >= 1.9.2)" do
-      Telegraph.text_to_morse("Añosé öresund").should == 
-      ".- --.-- --- ... ..-..       ---. .-. . ... ..- -. -.."
+      expect(Telegraph.text_to_morse("Añosé öresund")).to eq ".- --.-- --- ... ..-..       ---. .-. . ... ..- -. -.."
     end
-    
+
     it "undestands punctuation marks" do
-      Telegraph.text_to_morse(".,:?").should == ".-.-.- --..-- ---... ..--.."
+      expect(Telegraph.text_to_morse(".,:?")).to eq ".-.-.- --..-- ---... ..--.."
     end
-    
+
     it "undestands basic math operators" do
-      Telegraph.text_to_morse("3+100-(2x5)/10=299").should == 
-      "...-- .-.-. .---- ----- ----- -....- -.--. ..--- -..- ..... -.--.- -..-. .---- ----- -...- ..--- ----. ----."
+      expect(Telegraph.text_to_morse("3+100-(2x5)/10=299")).to eq(
+      "...-- .-.-. .---- ----- ----- -....- -.--. ..--- -..- ..... -.--.- -..-. .---- ----- -...- ..--- ----. ----.")
     end
-    
+
     it "can write email addresses" do
-      Telegraph.text_to_morse("morse@cod.es").should == "-- --- .-. ... . .--.-. -.-. --- -.. .-.-.- . ..."
+      expect(Telegraph.text_to_morse("morse@cod.es")).to eq "-- --- .-. ... . .--.-. -.-. --- -.. .-.-.- . ..."
     end
-    
+
     it "create a seven space separation between morse words" do
-      Telegraph.text_to_morse("a b").should == ".-       -..."
+      expect(Telegraph.text_to_morse("a b")).to eq ".-       -..."
     end
-    
+
     it "create a single space separation between morse letters" do
-      Telegraph.text_to_morse("ab").should == ".- -..."
+      expect(Telegraph.text_to_morse("ab")).to eq ".- -..."
     end
-    
+
     it "downcase every upcase letter" do
-      Telegraph.text_to_morse("NBA").should == Telegraph.text_to_morse("nbA")
+      expect(Telegraph.text_to_morse("NBA")).to eq Telegraph.text_to_morse("nbA")
     end
-    
+
     it "defines standard prosigns" do
-      Telegraph::Error.should_not be_empty
-      Telegraph::Understood.should_not be_empty
-      Telegraph::Invitation_to_transmit.should_not be_empty
-      Telegraph::Wait.should_not be_empty
-      Telegraph::End_of_work.should_not be_empty
-      Telegraph::Starting_signal.should_not be_empty
+      expect(Telegraph::Error).not_to be_empty
+      expect(Telegraph::Understood).not_to be_empty
+      expect(Telegraph::Invitation_to_transmit).not_to be_empty
+      expect(Telegraph::Wait).not_to be_empty
+      expect(Telegraph::End_of_work).not_to be_empty
+      expect(Telegraph::Starting_signal).not_to be_empty
     end
   end
-  
+
   describe 'Morse reader' do
     it "reads morse code" do
-      Telegraph.morse_to_text("... - .-. . . - ...       ..-. ..- .-.. .-..       --- ..-.       .-- .- - . .-. .-.-.-       .--. .-.. . .- ... .       .- -.. ...- .. ... . .-.-.-  ").should == "streets full of water. please advise."
+      expect(Telegraph.morse_to_text("... - .-. . . - ...       ..-. ..- .-.. .-..       --- ..-.       .-- .- - . .-. .-.-.-       .--. .-.. . .- ... .       .- -.. ...- .. ... . .-.-.-  ")).to eq "streets full of water. please advise."
     end
-    
+
     it "reads punctuation marks" do
-      Telegraph.morse_to_text("..--.. .-.-.- --..-- ---...").should == "?.,:"
+      expect(Telegraph.morse_to_text("..--.. .-.-.- --..-- ---...")).to eq "?.,:"
     end
-    
+
     it "reads basic math operators" do
-      Telegraph.morse_to_text("....- .-.-. .---- -----").should == "4+10"
+      expect(Telegraph.morse_to_text("....- .-.-. .---- -----")).to eq "4+10"
     end
-    
+
     it "takes seven spaces as words separation" do
-      Telegraph.morse_to_text(".       -").should == "e t"
+      expect(Telegraph.morse_to_text(".       -")).to eq "e t"
     end
-    
+
     it "takes a single space as letters separation" do
-      Telegraph.morse_to_text(". -").should == "et"
+      expect(Telegraph.morse_to_text(". -")).to eq "et"
     end
-    
+
     it "is consistent with the morse writer" do
       message = "A patient waiter is no loser."
-      Telegraph.text_to_morse(message).should == Telegraph.text_to_morse(Telegraph.morse_to_text(Telegraph.text_to_morse(message)))
+      expect(Telegraph.text_to_morse(message)).to eq Telegraph.text_to_morse(Telegraph.morse_to_text(Telegraph.text_to_morse(message)))
     end
   end
-  
+
 end
